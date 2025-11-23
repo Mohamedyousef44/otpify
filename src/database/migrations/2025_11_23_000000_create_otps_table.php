5e5f345e78c3b69b54503f81a3fa85aab706c07b
mohamed youssef <mohamed.youssef@objects.com>
2025-11-23 21:14:55 +0200

    create migration for otps table

---
+++ src/database/migrations/2025_11_23_000000_create_otps_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOtpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otps', function (Blueprint $table) {
+            $table->id();
+            $table->string('identifier');
+            $table->string('otp_hash');
+            $table->unsignedTinyInteger('attempts')->default(1);
+            $table->timestamp('expires_at');
+            $table->timestamps();
+
+            $table->index('identifier');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otps');
+    }
+}
